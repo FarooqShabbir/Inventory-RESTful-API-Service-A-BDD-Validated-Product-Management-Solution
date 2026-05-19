ad9154f2480f8df6a6dc37db79b016f0860e7d70
Farooq Shabbir <142401096+FarooqShabbir@users.noreply.github.com>
2026-05-19 18:19:20 +0500

    Create ProductFactory for test data generation
    Added a ProductFactory for generating fake product data for testing.

--- tests/factories.py
+++ tests/factories.py
@@ -1,1 +1,20 @@
 
+import factory
+from factory.fuzzy import FuzzyChoice, FuzzyDecimal
+from service.models import Product
+
+class ProductFactory(factory.Factory):
+    """ Creates fake products for testing """
+    class Meta:
+        model = Product
+
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("ecommerce_name")
+    category = FuzzyChoice(choices=["food", "tech", "clothing", "home"])
+    available = FuzzyChoice(choices=[True, False])
+    price = FuzzyDecimal(0.5, 1000.0)
+
+    @classmethod
+    def create_batch(cls, size, **kwargs):
+        """ Helper to create multiple fake products """
+        return super().create_batch(size, **kwargs)
